a12b88d6df5246a77e461eeaf56c7efbb62dc451
CB QPPMC S. TAVARES <21102159821@pm.df.gov.br>
2020-09-09 16:03:30 -0300

    primeiro comite de 09/09/2020

--- src/main/java/com/home/algafood/domain/repository/RestauranteRepository.java
+++ src/main/java/com/home/algafood/domain/repository/RestauranteRepository.java
@@ -8,7 +8,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.home.algafood.domain.model.Restaurante;
-import com.jayway.jsonpath.Option;
 
 @Repository
 public interface RestauranteRepository extends JpaRepository<Restaurante, Long> {
